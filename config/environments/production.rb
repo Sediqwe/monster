483f6ab48432fc21ae7e2f997886bb09efb15ba4
Sediqwe <sediqwe@gmail.com>
2022-02-03 22:21:58 +0100

    harmadik

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "gtav.sediqwe.eu"
+  config.hosts << "miracle.sediqwe.eu"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
