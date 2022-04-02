ff081d7c8a7df803fcdb325a91000bb7f6c1a2cb
Sediqwe <sediqwe@gmail.com>
2022-04-03 00:37:43 +0200

    dbase_rep

--- db/migrate/20220402193224_add_platform_to_upload.rb
+++ db/migrate/20220402193224_add_platform_to_upload.rb
@@ -1,6 +1,6 @@
 class AddPlatformToUpload < ActiveRecord::Migration[6.0]
   def change
-    add_column :uploads, :platform, :references
-    add_column :uploads, :program, :references
+    add_column :uploads, :platform, :text
+    add_column :uploads, :program, :text
   end
 end
