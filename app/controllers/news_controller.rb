29c1232fd00d935c5ce1aac060d1a699ecfdced3
Sediqwe <sediqwe@gmail.com>
2022-04-30 23:26:26 +0200

    főoldal 1.1

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,7 +3,7 @@ class NewsController < ApplicationController
 
   # GET /news or /news.json
   def index
-    @uploads= Upload.select(:datum).order('datum desc').group(:datum)
+    @uploads= Upload.select(:datum).order('datum desc').group(:datum).limit(30)
     
   end
 
